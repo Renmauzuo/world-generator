79eedaada34639f60c80071d29bff2498ec5df10
Renmauzuo <renmauzuo@yahoo.com>
2020-02-19 22:42:05 -0600

    Added Attributes
    Added basic attribute generation and editing.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,80 @@
+var rootNode;
+var selectedNode;
 
 $(function () {
     //TODO: Allow user to select a type of starting node if they only want to generate a single plane, world, continent, etc.
     //Start by creating a multiverse
-    detailsForNode(generateNode('multiverse')).appendTo('#generation-container');
+    rootNode = generateNode('multiverse');
+    domObjectForNode(rootNode).appendTo('#generation-container');
+
+    $('body').on('click', 'details,p', function (e) {
+        e.stopPropagation();
+        showInfoForNode($(this).data('node'));
+    });
+
+    $('#info-panel').on('input', 'input,select', function () {
+        var attribute = $(this).attr('id');
+        selectedNode.attributes[attribute] = $(this)[0].value;
+        //If its name changed then update the associated DOM element
+        if (attribute === 'name') {
+            var summaryText = objectTypes[selectedNode.type].typeName+' ('+$(this)[0].value+')';
+            if (selectedNode.children) {
+                selectedNode.domElement.children('summary').html(summaryText);
+            } else {
+                selectedNode.domElement.html(summaryText);
+            }
+        }
+    });
 });
+
+function showInfoForNode(node) {
+    selectedNode = node;
+    $info = $('#info-panel')
+    $info.empty();
+    $('<h2>'+objectTypes[node.type].typeName+'</h2>').appendTo($info);
+    if (node.attributes) {
+        for (var attribute in node.attributes) {
+            $('<label for="'+attribute+'">'+labels[attribute]+": </label>").appendTo($info);
+            //If the template attribute is an array create a picklist
+            var $input;
+            var templateAttribute = objectTypes[node.type].attributes[attribute];
+            if (Array.isArray(templateAttribute)) {
+                $input = $('<select id='+attribute+'></select>');
+                for (var index in templateAttribute) {
+                    $option = $('<option></option>').html(templateAttribute[index]).appendTo($input);
+                    //If this is the current value select it
+                    if (templateAttribute[index] === node.attributes[attribute]) {
+                        $option.attr('selected',true);
+                    }
+                }
+            //If it's not an array just check the type of the current value
+            } else if (typeof(node.attributes[attribute]) === "string") {
+                $input = $('<input type="text" id='+attribute+'>').attr('value',node.attributes[attribute]);
+            }
+            $input.appendTo($info);
+            $('<br>').appendTo($info);
+
+        }
+    }
+}
+
+/* Node Generation Begin */
 
 function generateNode(nodeType) {
     //Attributes and children will be generated on demand, so we need to remember if they've been generated or not
     var node = {
         type : nodeType,
-        needsChildren : objectTypes[nodeType].children != undefined,
-        needsAttributes : true
+        needsChildren : objectTypes[nodeType].children != undefined
     };
-    if (objectTypes[nodeType].name) {
-        if (typeof(objectTypes[nodeType].name) === 'function') {
-            node.name = objectTypes[nodeType].name();
-        } else {
-            node.name = randFromArray(objectTypes[nodeType].name)
+    if (objectTypes[nodeType].attributes) {
+        node.attributes = {};
+        var typeAttributes = objectTypes[nodeType].attributes
+        for (var attribute in typeAttributes) {
+            if (Array.isArray(typeAttributes[attribute])) {
+                node.attributes[attribute] = randFromArray(typeAttributes[attribute]);
+            } else if (typeof(typeAttributes[attribute]) === 'function') {
+                node.attributes[attribute] = typeAttributes[attribute]();   
+            }
         }
     }
     return node;
@@ -36,7 +94,7 @@ function onToggle() {
                 for (var i = 0; i<numChildren; i++) {
                     var childNode = generateNode(childType);
                     node.children.push(childNode);
-                    detailsForNode(childNode).appendTo($(this));
+                    domObjectForNode(childNode).appendTo($(this));
                 }
             }
         }
@@ -44,17 +102,33 @@ function onToggle() {
 }
 
 //Generates an HTML details object for a given node
-function detailsForNode(node) {
-    $details = $('<details></details>');
+function domObjectForNode(node) {
+    //If it has children then create a details group
     var summaryText = objectTypes[node.type].typeName;
-    if (node.name) {
-        summaryText+= ' ('+node.name+')';
+    if (node.attributes && node.attributes.name) {
+        summaryText+= ' ('+node.attributes.name+')';
     }
-    $('<summary></summary>').html(summaryText).appendTo($details);
-    $details.data('node', node);
-    $details.on('toggle', onToggle);
-    return $details;
+    var $domElement;
+    if (objectTypes[node.type].children) {
+        $domElement = $('<details></details>');
+        
+        $('<summary></summary>').html(summaryText).appendTo($domElement);
+        $domElement.on('toggle', onToggle);
+    } else {
+        //If it has no children then a simple p tag will do
+        $domElement = $('<p></p>').html(summaryText);
+    }
+    $domElement.data('node', node);
+    node.domElement = $domElement;
+    return $domElement;
 }
+
+/* Node Generation End */
+
+/* Data begin */
+
+//Common arrays that will be used by multiple objects
+var alignmentList =  ["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"];
 
 var objectTypes = {
     multiverse : {
@@ -68,12 +142,34 @@ var objectTypes = {
         children: {
             plane: [4, 20]
         },
-        name: planarClusterNameGenerator
+        attributes: {
+            name: planarClusterNameGenerator
+        }
     },
     plane : {
-        typeName : "Plane"
+        typeName : "Plane",
+        children: {
+            planarLayer: [1, 10]
+        },
+        attributes: {
+            alignment: alignmentList,
+            element: ["None", "Fire", "Air", "Water", "Earth", "Positive Energy", "Negative Energy"]
+        }
+    },
+    planarLayer : {
+        typeName: "Planar Layer"
     }
 };
+
+var labels = {
+    alignment: "Alignment",
+    element: "Element",
+    name: "Name"
+};
+
+/* Data End */
+
+/* Helpers Begin */
 
 function rand(min,max) {
     return Math.floor(Math.random() * (max-min) + min);
@@ -83,9 +179,19 @@ function randFromArray(array) {
     return array[rand(0, array.length-1)];
 }
 
+/* Helpers End */
+
+/* Name Generators Begin */
+
 function planarClusterNameGenerator() {
     var name = "The ";
     name+= randFromArray(['Outer', 'Inner', 'Forgotten', 'Distant', 'Esoteric', 'Unknown']) + ' ';
     name+= randFromArray(['Planes', 'Realms', 'Cluster', 'Lands']);
     return name;
 }
+
+function planarNameGenerator() {
+
+}
+
+/* Name Generators End */
